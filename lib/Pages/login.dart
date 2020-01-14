bdd2adc999fa9f6878228274b6fcc8d1903b9ce0
musumbidenis <musumbidenis@gmail.com>
2020-01-14 13:33:15 +0300

    Fetched academic years via API, commented my code

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -1,8 +1,7 @@
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:timetable/APIs/api.dart';
+import 'package:timetable/Pages/home.dart';
 import 'package:timetable/Pages/register.dart';
 
 class Login extends StatefulWidget {
@@ -12,93 +11,16 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
 
+GlobalKey <FormState> _formKey = GlobalKey();
+
+//Styling for texts//
+TextStyle style = TextStyle(fontFamily: "Montserrat", fontSize: 20.0);
+
+//Text Controllers//
 TextEditingController admissionController = TextEditingController();
 TextEditingController idController = TextEditingController();
 
-TextStyle style = TextStyle(fontFamily: "Montserrat", fontSize: 20.0);
 
-GlobalKey <FormState> _formKey = GlobalKey();
-
-Widget loginForm(){
-  return Column(
-  children: <Widget>[
-    CircleAvatar(
-      child: Image.asset("assets/hero.png"),
-      radius: 70.0,
-    ),
-    SizedBox(height: 25.0),
-    TextFormField(
-      controller: admissionController,
-      obscureText: false,
-      style: style,
-      keyboardType: TextInputType.text,
-      validator: (String value){
-        if (value.isEmpty) {
-          return "Admission number is required";
-        }else if (value.length != 13){
-          return "Admission number should contain 13 characters";
-        }else{
-          return null;
-        }
-      },
-      decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      hintText: "admission number",
-      border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    ),
-    SizedBox(height: 15.0),
-    TextFormField(
-      controller: idController,
-      obscureText: true,
-      style: style,
-      validator: (String value){
-        if (value.isEmpty){
-          return "Id number required";
-        }else if(value.length != 8){
-          return "Id number should contain 8 characters";
-        }else{
-          return null;
-        }
-      },
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(
-      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      hintText: "id number",
-      border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    ),
-    SizedBox(height: 25.0),
-    Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-      minWidth: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-      onPressed: _handleLogin,     
-      child: Text("Login",
-      textAlign: TextAlign.center,
-      style: style.copyWith(
-        color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    ),
-    SizedBox(height: 15.0),
-    Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text("Dont have an account?", style: TextStyle(color: Colors.black54),),
-        SizedBox(width: 10.0),
-        GestureDetector(
-          child: Text("Register", style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xff01A0C7),),),
-          onTap: () {
-        Navigator.push(context, MaterialPageRoute(
-          builder: (context) => Register()));
-          },
-        ),
-      ], 
-    ),
-  ],);
-}
 
 @override
 Widget build(BuildContext context) {
@@ -120,19 +42,130 @@ Widget build(BuildContext context) {
   );
 }
 
+
+
+////////////Login Form////////////
+Widget loginForm(){
+  return Column(
+  children: <Widget>[
+
+    /////Avatar image/////
+    CircleAvatar(
+      child: Image.asset("assets/hero.png"),
+      radius: 70.0,
+    ),
+
+    SizedBox(height: 25.0),
+
+    /////Admission Number text field/////
+    TextFormField(
+      controller: admissionController,
+      obscureText: false,
+      style: style,
+      keyboardType: TextInputType.text,
+      validator: (String value){
+        if (value.isEmpty) {
+          return "Admission number is required";
+        }else if (value.length != 13){
+          return "Admission number should contain 13 characters";
+        }else{
+          return null;
+        }
+      },
+      decoration: InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "admission number",
+      border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    ),
+    
+    SizedBox(height: 15.0),
+
+    /////Id Number text field/////
+    TextFormField(
+      controller: idController,
+      obscureText: true,
+      style: style,
+      validator: (String value){
+        if (value.isEmpty){
+          return "Id number required";
+        }else if(value.length != 8){
+          return "Id number should contain 8 characters";
+        }else{
+          return null;
+        }
+      },
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "id number",
+      border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    ),
+
+    SizedBox(height: 25.0),
+
+    /////Login button/////
+    Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+      minWidth: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      onPressed: _handleLogin,     
+      child: Text("Login",
+      textAlign: TextAlign.center,
+      style: style.copyWith(
+        color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    ),
+
+    SizedBox(height: 15.0),
+
+    /////Don't have an account ---> Register()/////
+    Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Dont have an account?", style: TextStyle(color: Colors.black54),),
+        SizedBox(width: 10.0),
+        GestureDetector(
+          child: Text("Register", style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xff01A0C7),),),
+          onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => Register()));
+          },
+        ),
+      ], 
+    ),
+  ],);
+}
+
+
+
+////////////Handling login authetication/////////////
 void _handleLogin() async{
   var form = _formKey.currentState;
-  if (form.validate()){
-    form.save();
+    if (form.validate()){
+      form.save();
   
+  //User data to be autheticated//
   var data = {
     'admission': admissionController.text,
     'idNumber': idController.text,
   };
 
+  //Check if the details are correct via API//
   var response = await CallAPi().postData(data,'login');
   var body = json.decode(response.body);
-  print(body);
+ 
+  if(body['Success'] != null){
+     print(body);
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) => Home()));
+  }else{
+    return print("errro");
   }
-}
+    
+   }
+  }
 }
--- lib/Pages/register.dart
+++ lib/Pages/register.dart
@@ -8,30 +8,37 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-String _mySelection;
-List data = List();
+
+String _course;
+int _year;
+
+List courses = List();
+List years = List();
 
 bool _isLoading = false;
 
 GlobalKey <FormState> _formKey = GlobalKey();
 
+///Styling for texts///
 TextStyle style = TextStyle(fontFamily: "Montserrat", fontSize: 20.0);
 
+///Text Controllers///
 TextEditingController admissionController = TextEditingController();
 TextEditingController nameController = TextEditingController();
 TextEditingController emailController = TextEditingController();
 TextEditingController idController = TextEditingController();
 TextEditingController phoneController = TextEditingController();
-TextEditingController courseController = TextEditingController();
-TextEditingController yearController = TextEditingController();
+
 
   @override
   void initState() {
     super.initState();
     this.getCourses();
+    this.getYears();
   }
 
-  @override
+
+
 Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -50,14 +57,22 @@ Widget build(BuildContext context) {
   }
 
 
+
+
+///////////Registration Form/////////////
 Widget registerForm(){
   return Column(
     children: <Widget>[
+
+      /////Avatar image/////
       CircleAvatar(
         child: Image.asset("assets/hero.png"),
         radius: 70.0,
       ),
+
       SizedBox(height: 25.0),
+
+      /////Admission Number text field/////
       TextFormField(
         controller: admissionController,
         obscureText: false,
@@ -77,7 +92,10 @@ Widget registerForm(){
         hintText: "admission number",
         border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
         ),
+
       SizedBox(height: 15.0),
+
+      /////Name text field/////
       TextFormField(
         controller: nameController,
         obscureText: false,
@@ -95,7 +113,10 @@ Widget registerForm(){
         hintText: "full name",
         border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
         ),
+
       SizedBox(height: 15.0),
+
+      /////Email text field/////
       TextFormField(
         controller: emailController,
         obscureText: false,
@@ -113,7 +134,10 @@ Widget registerForm(){
         hintText: "email address",
         border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
         ),
+
       SizedBox(height: 15.0),
+
+      /////Phone Number text field/////
       TextFormField(
         controller: phoneController,
         obscureText: false,
@@ -131,7 +155,10 @@ Widget registerForm(){
         hintText: "phone number",
         border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
         ),
+      
       SizedBox(height: 15.0),
+
+      /////Id Number text field/////
       TextFormField(
         controller: idController,
         obscureText: false,
@@ -149,49 +176,71 @@ Widget registerForm(){
         hintText: "id number",
         border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
         ),
-      SizedBox(height: 15.0),
-      SingleChildScrollView(child:
-      DropdownButton(
-        icon: Icon(Icons.arrow_drop_down),
-        iconSize: 10,
-        underline: Container(
-              height: 2,
-              color: Colors.deepPurpleAccent,
-            ),
-        items: data.map((item){
-          return DropdownMenuItem(
-            child: Text(item['courseTitle']),
-            value: item['courseCode'].toString(),
-          );
-        }).toList(),
-        onChanged: (newVal){
-          setState(() {
-            _mySelection = newVal;
-          });
-        },
-        value: _mySelection,
-      ),),
-      SizedBox(height: 15.0),
 
       SizedBox(height: 15.0),
-      TextFormField(
-        controller: yearController,
-        obscureText: false,
-        style: style,
-        keyboardType: TextInputType.number,
-        validator: (String value){
-          if (value.isEmpty) {
-            return "year of study is required";
-          }else{
-            return null;
-          }
-        },
-        decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: "year of study",
-        border:OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+
+      /////Courses dropdown/////
+      Container(
+        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(32.0),
+            border: Border.all(),
+          ),
+        child: DropdownButton(
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 10,
+          hint: Text("Select Course", style: style,),
+          items: courses.map((item){
+            return DropdownMenuItem(
+              child: Text(item['courseTitle'], style: style,),
+              value: item['courseCode'].toString(),
+            );
+          }).toList(),
+          onChanged: (newVal){
+            setState(() {
+              _course = newVal;
+            });
+          },
+          value: _course,
         ),
+      ),
+
+      SizedBox(height: 15.0),
+
+      /////Academic years dropdown/////
+      Container(
+        padding: EdgeInsets.only(left: 20.0, right: 90.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(32.0),
+            border: Border.all(),
+          ),
+        child: DropdownButton(
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 10,
+          hint: Text("Select Academic year", style: style,),
+          items: years.map((item){
+            return DropdownMenuItem(
+              child: Row(
+                children: <Widget>[
+                  Text("Academic year ", style: style,),
+                  Text(item['year'].toString(), style: style,),
+                ],
+              ),
+          value: item['yosId'],
+          );
+          }).toList(),
+          onChanged: (newVal){
+            setState(() {
+              _year = newVal;
+            });
+          },
+          value: _year,
+        ),
+      ),
+
       SizedBox(height: 25.0),
+
+      /////Registration button/////
       Material(
         elevation: 5.0,
         borderRadius: BorderRadius.circular(30.0),
@@ -206,7 +255,10 @@ Widget registerForm(){
             color: Colors.white, fontWeight: FontWeight.bold)),
         ),
       ),
+
       SizedBox(height: 15.0),
+
+      /////Already have an account ---> Login()/////
       Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -225,34 +277,53 @@ Widget registerForm(){
    );
   }
 
+////////////getCourses API////////////
 void getCourses() async{
-var response = await CallAPi().getData('getCourses');
-var body = json.decode(response.body);
-setState(() {
-  data = body;
-});
-
+  var response = await CallAPi().getData('getCourses');
+  var body = json.decode(response.body);
+  setState(() {
+    courses = body;
+  });
 }
+
+////////////getYears API////////////
+void getYears() async{
+  var response = await CallAPi().getData('getYears');
+  var body = json.decode(response.body);
+  setState(() {
+    years = body;
+  });
+}
+
+////////////Handling user registration///////////
 void _handleRegister() async{
   var form = _formKey.currentState;
   if (form.validate()){
     form.save();
-  
+
+  //User data to be pushed to db//
   var data = {
     'admission': admissionController.text,
     'name': nameController.text,
     'email': emailController.text,
     'idNumber': idController.text,
     'phone': phoneController.text,
-    'course': _mySelection,
-    'year': yearController.text,
+    'course': _course,
+    'year': _year
   };
+
+  //Set the registration button to loading state//
   setState(() {
     _isLoading = true;
   });
+
+  //Post user data to db via API//
   var response = await CallAPi().postData(data,'register');
   var body = json.decode(response.body);
   print(body);
   }
-}
+
+  //Return to login page
+  Navigator.pop(context);
+ }
 }
